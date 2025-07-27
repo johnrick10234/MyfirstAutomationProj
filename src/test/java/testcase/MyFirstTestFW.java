1de726232a8a8e7fe0dd2ff0a44a7946335e916d
johnr <johnr@DESKTOP-FK81N1S>
2025-07-26 20:04:31 -0500

    add username and pass to locators

--- src/test/java/testcase/MyFirstTestFW.java
+++ src/test/java/testcase/MyFirstTestFW.java
@@ -20,13 +20,13 @@ public class  MyFirstTestFW extends BaseTest {
 		
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		WebElement loginInput = wait.until(ExpectedConditions.elementToBeClickable(By.id(loc.getProperty("email_field"))));
-		loginInput.sendKeys("johnrickpogi23@gmail.com");
+		loginInput.sendKeys(loc.getProperty("zoho_email"));
 		
 		driver.findElement(By.xpath(loc.getProperty("next_button"))).click();
 		
 		WebElement loginPass = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(loc.getProperty("password_field"))));
 		
-		loginPass.sendKeys("Testonly1234!");
+		loginPass.sendKeys(loc.getProperty("zoho_pass"));
 //		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Testonly1234!");
 		driver.findElement(By.xpath(loc.getProperty("login_next_button"))).click();
 		
